6ffeb8c59e8f9c98ed1de5e361f4911a4d3acc2e
antonyoneill <antweb@gmail.com>
2017-04-30 15:16:43 +0100

    Remove unnecessary time difference code from the printer

--- src/tech/antonyoneill/antler/utils/PostPrinterImpl.java
+++ src/tech/antonyoneill/antler/utils/PostPrinterImpl.java
@@ -12,8 +12,7 @@ import tech.antonyoneill.antler.entity.Post;
  */
 public class PostPrinterImpl implements PostPrinter {
 
-    private static final String PRINT_PATTERN              = "%s - %s (%s)";
-    private static final String PRINT_PATTERN_WITHOUT_TIME = "%s - %s";
+    private static final String PRINT_PATTERN = "%s - %s (%s)";
 
     private TimeUtil            timeUtil;
 
@@ -33,19 +32,12 @@ public class PostPrinterImpl implements PostPrinter {
      */
     @Override
     public void printPosts(Collection<Post> posts) {
-        if (showTimeDifference()) {
-            posts.forEach((post) -> {
-                System.out.println(String.format(PRINT_PATTERN,
-                        post.getAuthor().getUsername(),
-                        post.getMessage(),
-                        timeUtil.timeDifference(post.getCreatedInstant())));
-            });
-        } else {
-            posts.forEach((post) -> {
-                System.out.println(
-                        String.format(PRINT_PATTERN_WITHOUT_TIME, post.getAuthor().getUsername(), post.getMessage()));
-            });
-        }
+        posts.forEach((post) -> {
+            System.out.println(String.format(PRINT_PATTERN,
+                    post.getAuthor().getUsername(),
+                    post.getMessage(),
+                    timeUtil.timeDifference(post.getCreatedInstant())));
+        });
     }
 
     /*
